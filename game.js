b459363417db709832fbde23a051e7555065bd93
fanchen <fanchen@jmgo.com>
2021-08-30 19:12:54 +0800

    node模块学习部分

---
+++ game.js
@@ -0,0 +1,36 @@
+//接收拼接进来的值
+
+module.exports = function(user) {
+    let random = Math.trunc(Math.random() * 4);
+    switch(random) {
+        case 1:
+            //石头
+            console.log('电脑出了石头')
+            break;
+        case 2:
+            //剪刀
+            console.log('电脑出了剪刀')
+            break;
+        case 3:
+            //布
+            console.log('电脑出了布')
+            break;
+    }
+    
+    if (user != 0 && user != 1 && user != 2) {
+        console.log(user,'请使用正确的出拳方式')
+    } else {
+        if(user === random) {
+            console.log('平局')
+        } else if(
+            user == 1 && random == 3 ||
+            user == 2 && random == 1 ||
+            user == 3 && random == 2
+        ) {
+            console.log('你输了')
+        } else {
+            console.log(random,'你赢了')
+        }
+    }
+}
+
--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const http = require('http')
+const url = require('url')
 
 http.createServer((request,response)=>{
+    const requestUrl = url.parse(request.url)
     response.writeHead(200);
-    response.end('hollow word')
+    if (requestUrl.pathname  == '/') {
+        console.log('/')
+    } else if(requestUrl.pathname  == '/game'){
+        console.log('/game')
+    }
 }).listen(8080,function(){
     console.log('服务端口已启动，请使用localhost:8080')
 })
+
+const game = require('./game.js')
+process.stdin.on('data',(buffer)=>{
+    const action = buffer.toString().trim()
+    game(action)
+})
+
